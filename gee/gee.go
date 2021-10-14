85d2828f4d46036aa677838846f2950d69b9c4c7
zhou <1051915419@qq.com>
2021-10-14 15:26:44 +0800

    first commit

---
+++ gee/gee.go
@@ -0,0 +1,48 @@
+package gee
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// HandlerFunc 定义一个请求路由方法
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+// Engine 这个引擎用来实现HTTPServer接口
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+// New 构造一个Engine，用来初始化
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
